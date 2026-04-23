067e57636c7341b34abb0e754f78d7ffbbce2926
acostanza <andy.costanza@betv.be>
2026-04-23 10:41:35 +0200

    fix: add explicit @service injections to NewsController, read sidebarTopics from model

--- assets/javascripts/discourse/controllers/news.js
+++ assets/javascripts/discourse/controllers/news.js
@@ -1,11 +1,22 @@
-import DiscoveryListController from 'discourse/controllers/discovery/list';
+import { service } from "@ember/service";
+import DiscoveryListController from "discourse/controllers/discovery/list";
 
 export default class NewsController extends DiscoveryListController {
+  @service site;
+  @service siteSettings;
+
   get showSidebar() {
     return this.showSidebarTopics && !this.site.mobileView;
   }
 
   get showSidebarTopics() {
-    return this.sidebarTopics && this.siteSettings.discourse_news_sidebar_topic_list;
+    return (
+      this.model?.sidebarTopics &&
+      this.siteSettings.discourse_news_sidebar_topic_list
+    );
+  }
+
+  get sidebarTopics() {
+    return this.model?.sidebarTopics ?? [];
   }
 }
